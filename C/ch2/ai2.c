2a1ace6c7ae51e5e26d687d54febb91336f3bf1c
KSato <ksato@KazuoSato.local>
2015-01-11 16:48:13 +0900

    escape keyを追加

--- C/ch2/ai2.c
+++ C/ch2/ai2.c
@@ -3,16 +3,16 @@
 //	2-gramが格納されたファイル2gram.txtを用います
 
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAXNO 100000 //2-gramの最大数
 #define MAXLINE 256 //1行の最大バイト数
 #define FILENAME "2gram.txt" //2-gramが格納されたファイル
 
 /*2-gramファイルの読み込み*/
-int read2gram(char db2gram[MAXNO][6])
+int read2gram(char db2gram[MAXNO][7])
 {
  FILE *fp;
  char line[MAXLINE] ;
@@ -24,22 +24,24 @@ int read2gram(char db2gram[MAXNO][6])
  }
 
  while(fgets(line,MAXLINE,fp)!=NULL){
-  strncpy(db2gram[i],line,4) ;//全角2文字をコピー
-  db2gram[i][5]='\0' ;
+  strncpy(db2gram[i],line,6) ;//全角2文字をコピー
+  db2gram[i][6]='\0' ;
   ++i ;
  }
  return i ;//2-gramの個数を返す
 }
+
 /*開始文字が何回含まれるか数える*/
-int findch(char *startch,char db2gram[MAXNO][6],int n) 
+int findch(char *startch,char db2gram[MAXNO][7],int n) 
 {
  int i ;
  int no=0 ;
  for(i=0;i<n;++i){
-  if(strncmp(startch,db2gram[i],2)==0) ++ no ;
+  if(strncmp(startch,db2gram[i],3)==0) ++ no ;
  }
  return no ;
 }
+
 /*num未満の乱数をセット*/
 int setrnd(int num)
 {
@@ -50,7 +52,7 @@ int setrnd(int num)
 }
 
 /*次の文字をn-gramによりランダムにセット*/
-void setrndstr(char *startch,char db2gram[MAXNO][6],int n)
+void setrndstr(char *startch,char db2gram[MAXNO][7],int n)
 {
  int i ;
  int point ;
@@ -58,16 +60,17 @@ void setrndstr(char *startch,char db2gram[MAXNO][6],int n)
  point=setrnd(n) ;//n未満の乱数をセット
  for(i=0;i<n;++i){
   if(i==point){
-   startch[0]=db2gram[i][2] ;
-   startch[1]=db2gram[i][3] ;
-   startch[2]='\0' ;
+   startch[0]=db2gram[i][3] ;
+   startch[1]=db2gram[i][4] ;
+   startch[2]=db2gram[i][5] ;
+   startch[3]='\0' ;
    break ;
   }
  }  
 }
 
 /*次の文字をn-gramによりセット*/
-void setnext(char *startch,char db2gram[MAXNO][6],int n,int num) 
+void setnext(char *startch,char db2gram[MAXNO][7],int n,int num) 
 {
  int i ;
  int no=0 ;
@@ -75,25 +78,27 @@ void setnext(char *startch,char db2gram[MAXNO][6],int n,int num)
  
  point=setrnd(num) ;//num未満の乱数をセット
  for(i=0;i<n;++i){
-  if(strncmp(startch,db2gram[i],2)==0) ++ no ;
+  if(strncmp(startch,db2gram[i],3)==0) ++ no ;
   if(no==point){
-   startch[0]=db2gram[i][2] ;
-   startch[1]=db2gram[i][3] ;
-   startch[2]='\0' ;
+   startch[0]=db2gram[i][3] ;
+   startch[1]=db2gram[i][4] ;
+   startch[2]=db2gram[i][5] ;
+   startch[3]='\0' ;
+   break ;
    break ;
   }
  }  
 }
 
-
 /*文の生成*/
-void generates(char *startch,char db2gram[MAXNO][6],int n)
+void generates(char *startch,char db2gram[MAXNO][7],int n)
 {
  int i,num ;
  
   /*開始文字を出力する*/
-  putchar(startch[0]) ; putchar(startch[1]) ;
+  putchar(startch[0]) ; putchar(startch[1]); putchar(startch[2]);
   /*句点が出るまで繰り返し*/
+
  do{
   /*開始文字が何回含まれるか数える*/
   num=findch(startch,db2gram,n) ;
@@ -103,15 +108,15 @@ void generates(char *startch,char db2gram[MAXNO][6],int n)
   else
    setrndstr(startch,db2gram,n) ;
   /*文字を出力する*/
-  putchar(startch[0]) ; putchar(startch[1]) ;
- }while((strncmp(startch,"．",2)!=0)&&(strncmp(startch,"。",2)!=0)) ;
+  putchar(startch[0]) ; putchar(startch[1]); putchar(startch[2]);
+ }while((strncmp(startch,"．",3)!=0)&&(strncmp(startch,"。",3)!=0)) ;
  printf("\n") ;
 }
 
 int main()
 {
  char line[MAXLINE] ;//入力バッファ
- char db2gram[MAXNO][6] ;//2gramのデータベース
+ char db2gram[MAXNO][7] ;//2gramのデータベース
  int n ;//2-gramの個数
  char startch[MAXLINE];//開始文字
  
@@ -120,14 +125,20 @@ int main()
  srand(65535) ;
  /*2-gramファイルの読み込み*/
  n=read2gram(db2gram) ;
-
+ int num = 0; 
  /*オープニングメッセージ*/
  printf("さくら：メッセージをどうぞ\n");
  printf("あなた：");
  /*会話しましょう*/
- while(fgets(line,MAXLINE,stdin)!=NULL){
+ while(fgets(line,MAXLINE,stdin)!=NULL ){
+
+  if(strlen(line) < 3 && line[0] == 'q'){
+    break;
+  }
+
   printf("さくら：");
-  strncpy(startch,&(line[setrnd((strlen(line)-1)/2)*2]),2) ;
+  num = setrnd((strlen(line)-1)/3)*3;
+  strncpy(startch,&(line[num]),3) ;
   generates(startch,db2gram,n) ;
   printf("あなた：");
  }
